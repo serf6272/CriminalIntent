8c30c70b782f7b2f7c2a3c1590e891b6f1264db4
serf <serf6272@gmail.com>
2022-07-16 12:00:23 +0300

    Commit 7 list 395 ebook

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
 import java.util.*
@@ -17,9 +18,9 @@ class CrimeRepository private constructor(context: Context) {
 
     private val crimeDao = database.crimeDao()
 
-    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
 
-    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
 
     companion object{
         private var INSTANCE: CrimeRepository? = null
